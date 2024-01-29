8c8c81df7112ab89441e37eb6802383829540cf9
Thiago NR <thiagokroos55gmail.com>
2024-01-29 19:19:00 -0300

    importando funções

---
+++ desafio 1/desafio/start.js
@@ -0,0 +1,32 @@
+const Module= require('./random.js')
+
+
+ function inicio(){
+    
+    const caixaN = [];
+
+  function  fillArrayWithRandomNumbers(caixaN){}
+    console.log('Array antes da ordenação:', caixaN);
+
+
+  function  sortArray(caixaN){};
+
+  
+    console.log('Array após ordenação crescente:', caixaN);
+
+    
+    fillArrayWithRandomNumbers(caixaN);
+
+    console.log('Array antes da ordenação:', caixaN);
+
+    
+    sortArray(caixaN, 'desc');
+
+    console.log('Array após ordenação decrescente:', caixaN);
+
+}    
+ 
+
+console.log(Module.fillArrayWithRandomNumbers)
+Module.sortArray()
+inicio()
